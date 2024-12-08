18e7c832208fa85261bc467a9d83583ad9f18a08
junaid khalid <junaidkhalid2001@gmail.com>
2024-12-08 16:16:53 +0500

    development completed

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Access denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token expired, please log in again" });
+    }
+    
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
+
+export default protect;
